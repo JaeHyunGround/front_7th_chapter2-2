023e669af1cfe14bf580c9eb5cf9770420f76784
JaeHyunGround <gothddlek@naver.com>
2025-11-18 21:39:07 +0900

    feat : renderElement 함수 작성 (첫 렌더링)

--- src/lib/renderElement.js
+++ src/lib/renderElement.js
@@ -1,10 +1,26 @@
 import { setupEventListeners } from "./eventManager";
 import { createElement } from "./createElement";
 import { normalizeVNode } from "./normalizeVNode";
-import { updateElement } from "./updateElement";
 
 export function renderElement(vNode, container) {
-  // 최초 렌더링시에는 createElement로 DOM을 생성하고
-  // 이후에는 updateElement로 기존 DOM을 업데이트한다.
-  // 렌더링이 완료되면 container에 이벤트를 등록한다.
+  // vNode를 정규화 한 다음에
+  const normalized = normalizeVNode(vNode);
+
+  // 처음 렌더링인지 확인
+  const isFirstRender = !container._eventListenersSetup;
+
+  // container 초기화
+  container.innerHTML = "";
+
+  // createElement로 노드를 만들고
+  const element = createElement(normalized);
+
+  // container에 삽입하고
+  container.appendChild(element);
+
+  // 이벤트를 등록합니다. (처음 렌더링 시에만)
+  if (isFirstRender) {
+    setupEventListeners(container);
+    container._eventListenersSetup = true;
+  }
 }
