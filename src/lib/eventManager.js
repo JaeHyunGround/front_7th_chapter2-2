0e5e0087c3641535282a7e8b73db6b97eb5968a3
JaeHyunGround <gothddlek@naver.com>
2025-11-18 20:37:32 +0900

    feat : 이벤트 관리 함수 작성

--- src/lib/eventManager.js
+++ src/lib/eventManager.js
@@ -1,5 +1,58 @@
-export function setupEventListeners(root) {}
+const registeredEvent = new Map();
 
-export function addEvent(element, eventType, handler) {}
+export function setupEventListeners(root) {
+  const eventTypes = Array.from(registeredEvent.keys());
 
-export function removeEvent(element, eventType, handler) {}
+  eventTypes.forEach((type) => {
+    root.addEventListener(type, (e) => {
+      const eventList = registeredEvent.get(type);
+      eventList.forEach(({ element, handler }) => {
+        if (e.target === element) handler(e);
+      });
+    });
+  });
+}
+
+export function addEvent(element, eventType, handler) {
+  //   if (!registeredEvent.has(eventType)) {
+  //     registeredEvent.set(eventType, [
+  //       {
+  //         element,
+  //         handler,
+  //       },
+  //     ]);
+  //   } else {
+  //     registeredEvent.set(eventType, [
+  //       ...registeredEvent.get(eventType),
+  //       {
+  //         element,
+  //         handler,
+  //       },
+  //     ]);
+  //   }
+
+  if (!registeredEvent.has(eventType)) {
+    registeredEvent.set(eventType, []);
+  }
+
+  // Map에 저장된 배열은 참조이기 때문에, 배열을 직접 수정해도 Map에 반영됨
+  registeredEvent.get(eventType).push({
+    element,
+    handler,
+  });
+}
+
+export function removeEvent(element, eventType, handler) {
+  if (!registeredEvent.has(eventType)) return;
+
+  const events = registeredEvent.get(eventType);
+  const filteredEvents = events.filter(
+    (e) => e.element !== element || e.handler !== handler,
+  );
+
+  if (filteredEvents.length === 0) {
+    registeredEvent.delete(eventType);
+  } else {
+    registeredEvent.set(eventType, filteredEvents);
+  }
+}
